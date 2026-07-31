06be59b9825c0999e9c256be400c35957d905fe8
BabyYann <BabyYann@users.noreply.github.com>
2026-08-01 00:16:33 +0700

    fix(academic): add missing Pagination component import in Students/Index.vue

--- resources/js/Pages/Academic/Students/Index.vue
+++ resources/js/Pages/Academic/Students/Index.vue
@@ -6,6 +6,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Pagination from '@/Components/Pagination.vue';
 import { 
     MagnifyingGlassIcon, FunnelIcon, ArrowPathIcon, ArrowUpTrayIcon, 
     PlusIcon, UserIcon, EnvelopeIcon, EyeIcon, PencilSquareIcon, 
